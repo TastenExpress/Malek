b5137a048ac725b9d6edd175fe2e6783d33c5d16
WaqasWZ <waqas12394941@gmail.com>
2022-02-04 16:19:06 +0500

    Error in Manifest removed

--- ecommerce_store/__manifest__.py
+++ ecommerce_store/__manifest__.py
@@ -21,7 +21,7 @@
     'assets': {
         'web.assets_frontend': [
             'ecommerce_store/static/src/js/checkout_customers.js',
-            'ecommerce_store/static/src/scss/custom_shop.scss'
+            'ecommerce_store/static/src/scss/custom_shop.scss',
             "ecommerce_store/static/src/css/custom.css",
             'ecommerce_store/static/src/js/checkout_customers.js'
         ],
