379e8837b355320c5853f139ea993458f5f80139
Huzaifa Ahmed <44926606+huxe@users.noreply.github.com>
2022-03-17 18:13:29 +0500

    Update __init__.py

--- sh_qty_shop_ajax_moq/__init__.py
+++ sh_qty_shop_ajax_moq/__init__.py
@@ -2,3 +2,8 @@
 
 from . import models
 from . import controllers
+from odoo import api,SUPERUSER_ID
+
+def add_packaging_qty_website(cr,registry):
+    env=api.Environment(cr,SUPERUSER_ID,{})
+    env['product.template'].productloop()
