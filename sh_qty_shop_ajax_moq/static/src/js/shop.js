904a92b9cc78f78ca8cbce148acc5b1325ec664f
Huzaifa Ahmed <44926606+huxe@users.noreply.github.com>
2022-03-18 21:56:28 +0500

    Update shop.js

--- sh_qty_shop_ajax_moq/static/src/js/shop.js
+++ sh_qty_shop_ajax_moq/static/src/js/shop.js
@@ -46,7 +46,8 @@ odoo.define("sh_qty_shop_ajax_moq.website_sale", function (require) {
                 productId = parseInt(productId, 10);
 
                 if (productId) {
-                    console.log($el.closest("form"));
+                    console.log($el.closest("form").find(".quantity").val() || 1.0);
+                    console.log($el.closest("form").find(".setqty").val() || 1.0);
                     return self
                         ._rpc({
                             route: "/shop/cart/update_json",
@@ -70,7 +71,7 @@ odoo.define("sh_qty_shop_ajax_moq.website_sale", function (require) {
                 }
             });
         },
-       
+    
         _onClickAddCartJSON: function (ev) {
             ev.preventDefault();
             console.log("_onClickAddCartJSON");
