f3824db774cb64249d6ac6f40beb868b13e26025
Zeeshan <59208313+ZeeshanArif8@users.noreply.github.com>
2022-03-23 13:25:50 +0500

    Update sale_main.py

--- ecommerce_store/controllers/sale_main.py
+++ ecommerce_store/controllers/sale_main.py
@@ -4,18 +4,22 @@ from odoo.http import request
 from odoo.exceptions import UserError
 from odoo.addons.website_sale.controllers.main import WebsiteSale
 
-# class WebsiteSaleInheritSale(WebsiteSale):
+class WebsiteSaleInheritSale(WebsiteSale):
 
-#     @http.route()
-#     def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None, display=True, **kw):
-#         product_packages = request.env["product.packaging"].sudo().search([('product_id','=',product_id)],order='qty')
-#         # raise UserError(str(add_qty))
+    @http.route()
+    def cart_update_json(self, product_id, line_id=None, add_qty=None, set_qty=None, display=True, **kw):
+        product_packages = request.env["product.packaging"].sudo().search([('product_id','=',product_id)],order='qty')
+#         raise UserError('product_id: '+str(product_id)+'line_id: '+str(line_id)+'add_qty: '+str(add_qty)+'set_qty: '+str(set_qty)+str(type(set_qty)))        
+        if line_id:
+            add_qty= None
+            set_qty= 0
+        else:
+            sdd_qty=int(add_qty)
+            
+#         raise UserError('product_id: '+str(product_id)+'line_id: '+str(line_id)+'add_qty: '+str(add_qty)+'set_qty: '+str(set_qty)+str(type(set_qty)))
+        res = super(WebsiteSaleInheritSale, self).cart_update_json(product_id, line_id, add_qty, set_qty, display, **kw)
+        return res
 
-#         if not add_qty:
-#             add_qty = product_packages[0].qty
-
-#         res = super(WebsiteSaleInheritSale, self).cart_update_json(product_id, line_id, add_qty, set_qty, display, **kw)
-#         return res
 
 
 class WebsiteSaleInherit(http.Controller):
